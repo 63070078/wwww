0bb6640a71b8450d6da1c4b42f083ad596d7af22
tanatporn <73688566+tanatporn@users.noreply.github.com>
2020-10-30 15:58:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
-<?php
-echo "<h2>PHP is fun!</h2>";
-echo "Hello world!<br>";
-?>
+<html>
+    <head>
+        <title>Hi doctor</title>
+    </head>
+    <body>
+        <h1>Tanatporn Ratsamisuriya Ploy</h1>
+        <p></p>
+        <img src="meow1.jpg">
+        <img src="meow2.jpg">
+        <a href="https://www.youtube.com">This is a Link</a>
+
+        <h2>เรื่องตลก</h2>
+        <p>ณ โรงพยาบาลแห่งหนึ่ง
+
+            หมอ : โดนไรมา
+            
+            คนไข้ : โดนแตนต่อย
+            
+            หมอ : ต่อยนานยัง
+            
+            คนไข้ : พึ่งต่อย
+
+            หมอ : ตกลงอะไรต่อยกันแน่
+
+            คนไข้ : แตนต่อย
+
+            หมอ : ต่อยนานยัง
+
+            คนไข้ : พึ่งต่อย
+
+            หมอ : ตกลงอะไรต่อยโว้ยยย
+
+            คนไข้ : แตนต่อย
+
+            หมอ : ต่อยนานยัง
+
+            คนไข้ : ผมจะต่อยหมอนี่แหละ
+
+
+            
+        </p>
+    </body>
+</html>
