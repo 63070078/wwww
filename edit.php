e91781d1daa21cc9649bafa9e33a4c05545c370d
63070078 <73688566+63070078@users.noreply.github.com>
2020-11-27 05:20:53 +0700

    Create edit.php

---
+++ edit.php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'lastlab.mysql.database.azure.com', 'it63070078@lastlab', 'FOFcxd53', 'ITFLab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL PLEASE TRY AGAIN: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+
+
+$sql = "UPDATE guestbook SET Comment ='$comment' WHERE Name='$name' ";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "Edited successfully!";
+    echo '<a href="https://nidaweb.azurewebsites.net/show.php"> click here to open first page </a>';
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
