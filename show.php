0e12119fe5849c0043979382ff1dca62a717cf13
63070078 <73688566+63070078@users.noreply.github.com>
2020-11-27 05:00:02 +0700

    Update show.php

--- show.php
+++ show.php
@@ -15,7 +15,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'pimpimchanok119.mysql.database.azure.com', 'Pimchanok119@pimpimchanok119', 'PPNuro26', 'itflab', 3306);
+mysqli_real_connect($conn, 'lastlab.mysql.database.azure.com', 'it63070078@lastlab', 'FOFcxd53', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL PLEASE TRY AGAIN !: '.mysqli_connect_error());
